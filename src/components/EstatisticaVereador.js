1382a8e07d2e9da9f8a5ae61d8b853f6d9221d0b
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2018-12-16 17:57:45 -0200

    Ajustar dados de estatisticas dos vereadores

--- src/components/EstatisticaVereador.js
+++ src/components/EstatisticaVereador.js
@@ -6,16 +6,19 @@ import { Divider } from 'semantic-ui-react'
 const EstatisticaVereador = (props) => (
   <div>
     <Divider horizontal>Solicitações</Divider>
-    <Statistic.Group widths='two'>
-      <Statistic size='mini'>
-        <Statistic.Label>Projetos</Statistic.Label>
+    <Statistic.Group horizontal>
+      <Statistic>
+        <Statistic.Value>{props.dados.legislaturas.quantidade}</Statistic.Value>
+        <Statistic.Label>Anos de Legislaturas</Statistic.Label>
+      </Statistic>
+      <Statistic>
         <Statistic.Value>{props.dados.projetos.quantidade}</Statistic.Value>
-      </Statistic>    
-
-      <Statistic size='mini'>
+        <Statistic.Label>Projetos Apresentados</Statistic.Label>
+      </Statistic>
+      {/* <Statistic>
+        <Statistic.Value>X</Statistic.Value>
         <Statistic.Label>Indicações</Statistic.Label>
-        <Statistic.Value>0</Statistic.Value>
-      </Statistic>   
+      </Statistic> */}
     </Statistic.Group>
   </div>
 )
