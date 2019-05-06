bb08001918f9f6edbc59bca0e1590ac9af5fffc9
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-05-05 21:14:14 -0300

    Inserir label de informação de quando as indicações foram iniciadas

--- src/components/EstatisticaVereador.js
+++ src/components/EstatisticaVereador.js
@@ -18,9 +18,14 @@ const EstatisticaVereador = (props) => (
       <Statistic>
         <Statistic.Value>{props.dados.indicacoes.length}</Statistic.Value>
         <Statistic.Label>Indicações</Statistic.Label>
+        <Statistic.Label style={label_indicacoes}>(A partir de 2018)</Statistic.Label>
       </Statistic>
     </Statistic.Group>
   </div>
 )
+
+const label_indicacoes = {
+  fontSize: '8px',
+}
 
 export default EstatisticaVereador
