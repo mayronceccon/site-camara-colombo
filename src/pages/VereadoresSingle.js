3f2c2f4cc74e6abe383e5578e111a477f06a7ad3
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-05-21 20:35:42 -0300

    Ajuste na busca dos dados do vereador na API

--- src/pages/VereadoresSingle.js
+++ src/pages/VereadoresSingle.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 class VereadoresSingle extends Component {
   constructor(props) {
@@ -6,35 +7,35 @@ class VereadoresSingle extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      vereador: [],
     };
   }
 
   componentDidMount() {
-    const { id } = this.props.match.params
-    fetch(`https://camaracolombo.com.br:5005/vereadores/${id}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.results
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    this.carregarVereadores();    
   }
 
+  carregarVereadores = async () => {
+    const { id } = this.props.match.params
+    await api.get(`/vereadores/${id}`).then(response => {
+      console.log(response.data);
+      this.setState({
+        isLoaded: true,
+        vereador: response.data,
+      });
+    }).catch(error => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    });
+  };
 
   render() {
     return (
       <div>
-        <h1>Vereador</h1>  
+        <h1>Vereador</h1>
+        {this.state.vereador.nome}
       </div>
     );
   }
