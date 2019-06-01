0a807c727ad6f22c54f8c3c50b45e3759d178f56
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-06-01 17:24:33 -0300

    Add lib de cache

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { setupCache } from 'axios-cache-adapter'
+
+const cache = setupCache({
+  maxAge: 60 * 60 * 1000 //60 minutos
+})
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL_API
+  baseURL: process.env.REACT_APP_URL_API,
+  adapter: cache.adapter
 });
 
 export default api;
