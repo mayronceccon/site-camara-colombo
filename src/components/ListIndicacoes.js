6501827793d1685adbba9ec56ff0f0d9dc08a331
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-06-03 16:13:53 -0300

    Add formulário de pesquisa de indicações

--- src/components/ListIndicacoes.js
+++ src/components/ListIndicacoes.js
@@ -5,6 +5,7 @@ import {
   Dimmer,
   Loader,
   Table,
+  Form,
 } from 'semantic-ui-react'
 import api from '../services/api';
 import MyPaginacao from '../components/MyPaginacao';
@@ -17,11 +18,79 @@ export default class ListIndicacoes extends Component {
         isLoaded: null,
         items: [],
         indicacoes: [],
+        pesquisarCampo: 'assunto',
+        pesquisarValor: '',
+        isVereador: false,
       };
     }
     
     componentDidMount() {
       this.carregarIndicacoes();      
+    }   
+
+    handleCampos = (event, { name, value }) => {
+      this.setState({[name]: value});
+    }
+
+    handlePesquisar = (event) => {
+      event.preventDefault();
+      let campo = this.state.pesquisarCampo;
+      let valor = this.state.pesquisarValor;
+
+      // let d = campos.map((k, v) => {
+      //   return `${k}=${valor}`;
+      // });
+
+      // let u = d.join('&');
+      // let url = `/indicacoes/?${u}`;
+
+      let url = `/indicacoes/?${campo}=${valor}`;
+      
+      this.carregarIndicacoes(url);
+    }
+
+    InputGetOption = () => {
+      if (this.state.isVereador) {
+        let options = [
+          { text: 'Assunto', value: 'assunto' },
+          { text: 'Destinatário', value: 'destinatario' },
+        ]
+        return options;
+      }
+      let options = [
+        { text: 'Assunto', value: 'assunto' },
+        { text: 'Vereador', value: 'vereador' },
+        { text: 'Destinatário', value: 'destinatario' },
+      ]
+      return options;
+    }
+    
+    InputPesquisar = () => {
+      let options = this.InputGetOption();
+      
+      const {pesquisarValor, pesquisarCampo} = this.state
+       
+      return (
+        <Form onSubmit={this.handlePesquisar}>
+          <Form.Group>
+            <Form.Input
+              name='pesquisarValor'
+              placeholder='Pesquisar...'
+              value={pesquisarValor}
+              onChange={this.handleCampos}
+              required
+            />
+            <Form.Select
+              name='pesquisarCampo'
+              options={options} 
+              value={pesquisarCampo}
+              onChange={this.handleCampos}
+              required
+            />
+            <Form.Button content='Pesquisar' />
+          </Form.Group>
+        </Form>
+      )
     }
     
     carregarIndicacoes = async (url) => {
@@ -30,6 +99,13 @@ export default class ListIndicacoes extends Component {
       })
       
       const { id } = this.props.match.params;
+
+      if (id !== undefined) {
+        this.setState({
+          isVereador: true,
+        })
+      }
+      
       if (url === undefined && id !== undefined) {
         url = `/indicacoes/?vereador=${id}`;
       }
@@ -37,7 +113,10 @@ export default class ListIndicacoes extends Component {
       if (url === undefined && id === undefined) {
         url = `/indicacoes/`;
       }
-
+      
+      // if (url !== undefined && id !== undefined) {
+      //   url = `${url}&vereador=${id}`;
+      // }
       await api.get(url, {
         cache: {
           maxAge: 60 * 60 * 1000, // 60 minutos
@@ -75,35 +154,38 @@ export default class ListIndicacoes extends Component {
         );
       } else {
         return (
-          <Table celled textAlign='center' verticalAlign='middle'>
-            <Table.Header fullWidth>
-              <Table.Row>
-                <Table.HeaderCell>Número</Table.HeaderCell>
-                <Table.HeaderCell>Data da Pauta</Table.HeaderCell>
-                <Table.HeaderCell>Assunto</Table.HeaderCell>
-                <Table.HeaderCell>Vereador</Table.HeaderCell>
-                <Table.HeaderCell>Destinatário</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {indicacoes.map(item => (                  
-                <Table.Row key={item.id}>
-                  <Table.Cell>{item.numero}</Table.Cell>
-                  <Table.Cell>{this.formatData(item.pauta.data_sessao)}</Table.Cell>
-                  <Table.Cell>{item.assunto}</Table.Cell>
-                  <Table.Cell>{item.vereador.nome}</Table.Cell>
-                  <Table.Cell>{item.destinatario.nome}</Table.Cell>
-                </Table.Row>              
-              ))}
-            </Table.Body>
-            <Table.Footer fullWidth>
-              <Table.Row textAlign='center' verticalAlign='middle'>
-                <Table.HeaderCell colSpan='5'>                  
-                  <MyPaginacao items={items} onClick={this.carregarIndicacoes}></MyPaginacao>                  
-                </Table.HeaderCell>
-              </Table.Row>
-            </Table.Footer>
-          </Table>
+          <div>
+            { this.InputPesquisar() }
+            <Table celled textAlign='center' verticalAlign='middle'>
+              <Table.Header fullWidth>
+                <Table.Row>
+                  <Table.HeaderCell>Número</Table.HeaderCell>
+                  <Table.HeaderCell>Data da Pauta</Table.HeaderCell>
+                  <Table.HeaderCell>Assunto</Table.HeaderCell>
+                  <Table.HeaderCell>Vereador</Table.HeaderCell>
+                  <Table.HeaderCell>Destinatário</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {indicacoes.map(item => (                  
+                  <Table.Row key={item.id}>
+                    <Table.Cell>{item.numero}</Table.Cell>
+                    <Table.Cell>{this.formatData(item.pauta.data_sessao)}</Table.Cell>
+                    <Table.Cell>{item.assunto}</Table.Cell>
+                    <Table.Cell>{item.vereador.nome}</Table.Cell>
+                    <Table.Cell>{item.destinatario.nome}</Table.Cell>
+                  </Table.Row>              
+                ))}
+              </Table.Body>
+              <Table.Footer fullWidth>
+                <Table.Row textAlign='center' verticalAlign='middle'>
+                  <Table.HeaderCell colSpan='5'>                  
+                    <MyPaginacao items={items} onClick={this.carregarIndicacoes}></MyPaginacao>                  
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
+            </Table>
+          </div>
         );
       }
     }
