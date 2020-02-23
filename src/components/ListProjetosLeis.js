3ac813e7c8a355ca0cb0fe5c281a5e65f34e0d8a
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-23 12:35:27 -0300

    Alterar forma de busca dos projetos para nova API

--- src/components/ListProjetosLeis.js
+++ src/components/ListProjetosLeis.js
@@ -4,17 +4,9 @@ import {
   Dimmer, 
   Loader, 
   Icon, 
-  Pagination 
 } from 'semantic-ui-react'
-import _ from 'lodash'
-
-
-function statusProjeto(data) {
-  if (data) {
-    return <Icon color='green' name='check' />;
-  }
-  return <Icon color='red' name='times' />;
-}
+import api from '../services/api';
+import MyPaginacao from '../components/MyPaginacao';
 
 export default class ListProjetosLeis extends Component {
     constructor(props) {
@@ -23,103 +15,58 @@ export default class ListProjetosLeis extends Component {
         error: null,
         isLoaded: false,
         items: [],
-        paginatedItems: [],
-        column: null,
-        direction: null,
-        limit: 25,
-        paginas: null,
-        paginaAtiva: 1,
-        filtro: false,
+        projetos: [],
       };
-    }
-
-    handleSort = clickedColumn => () => {
-      const { column, items, direction } = this.state
-  
-      if (column !== clickedColumn) {
-        let data = _.sortBy(items, [clickedColumn]);
-        this.setState({
-          column: clickedColumn,
-          items: data,
-          direction: 'ascending',
-        });
-
-        this.getPaginatedItems(data);
-  
-        return
-      }
-      let data = items.reverse();
-      this.setState({
-        items: data,
-        direction: direction === 'ascending' ? 'descending' : 'ascending',
-      })
-      this.getPaginatedItems(data);
-    }
-
-    filterList = (event) => {
-      let updatedList = this.state.items;
-      updatedList = updatedList.filter(function(item){
-        return item.assunto.toLowerCase().search(
-          event.target.value.toLowerCase()) !== -1;
-      });
-      this.setState({
-        filtro: true,
-      })
-
-      this.getPaginatedItems(updatedList);
     }
   
     componentDidMount() {
-      fetch("https://api.cidadaonacamara.com.br/projetos_lei/")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            let items = result.results;
-            this.getPaginatedItems(items);
-            this.setState({
-              isLoaded: true,
-              items: items,
-            });
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
+      this.carregarDados()
     }
 
-    handlePageClick = (event, data) => {
-      let page = data.activePage;
-      let dados = this.state.items;
-      // if (this.state.filtro) {
-      //   dados = this.state.
-      // }
-      this.getPaginatedItems(dados, page);
-    };
-
-    getPaginatedItems = (items, page) => {
-      page = page || 1;
-      let limit = this.state.limit;
-      let offset = (page - 1) * limit;
-      let paginatedItems = _(items)
-                .drop(offset)
-                .take(limit)
-                .value();
-
+    carregarDados = async (url) => {
       this.setState({
-        paginas: Math.ceil(items.length / limit),
-        paginatedItems: paginatedItems,
-        paginaAtiva: page,
+        isLoaded: false,
+      })
+
+      if (url === undefined) {
+        url = `projetos/`;
+      }
+
+      await api.get(url, {
+        cache: {
+          maxAge: 60 * 60 * 1000, // 60 minutos
+          exclude: { query: false }
+        }
+      }).then(response => {
+        this.setState({
+          isLoaded: true,
+          items: response.data,
+          projetos: response.data.results
+        });
+      }).catch(error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
       });
     }
 
+    getVereador = (item) => {
+      if (item.vereador === null) {
+        return ""
+      }
+      return item.vereador.nome
+    }
+
+    statusProjeto = (data) => {
+      if (data) {
+        return <Icon color='green' name='check' />;
+      }
+      return <Icon color='red' name='times' />;
+    }
+
     render() {
-      const { error, isLoaded, paginatedItems, column, direction } = this.state;
+      const { error, isLoaded, projetos, items } = this.state;
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
@@ -131,62 +78,35 @@ export default class ListProjetosLeis extends Component {
       } else {
         return (
           <div>
-            {/* <Form>
-              <Form.Group widths='equal'>
-                <Form.Input fluid label='Pesquisa' placeholder='Pesquisa' onChange={e => this.filterList(e)} />
-              </Form.Group>
-            </Form> */}
-            <Table striped sortable celled fixed>
-              <Table.Header>
+            <Table celled textAlign='center' verticalAlign='middle'>
+              <Table.Header fullWidth>
                 <Table.Row>
-                  <Table.HeaderCell 
-                    textAlign='center'
-                    sorted={column === 'projeto' ? direction : null}
-                    onClick={this.handleSort('projeto')}
-                  >Nº</Table.HeaderCell>
-                  <Table.HeaderCell
-                    sorted={column === 'assunto' ? direction : null}
-                    onClick={this.handleSort('assunto')}
-                  >Assunto</Table.HeaderCell>
-                  <Table.HeaderCell
-                    sorted={column === 'vereador.nome' ? direction : null}
-                    onClick={this.handleSort('vereador.nome')}
-                  >Vereador</Table.HeaderCell>
-                  <Table.HeaderCell 
-                    textAlign='center'
-                    sorted={column === 'data_aprovacao' ? direction : null}
-                    onClick={this.handleSort('data_aprovacao')}
-                  >Aprovado</Table.HeaderCell>
-                  <Table.HeaderCell 
-                    textAlign='center'
-                    sorted={column === 'data_arquivamento' ? direction : null}
-                    onClick={this.handleSort('data_arquivamento')}
-                  >Arquivado</Table.HeaderCell>
+                  <Table.HeaderCell>Número</Table.HeaderCell>
+                  <Table.HeaderCell>Assunto</Table.HeaderCell>
+                  <Table.HeaderCell>Vereador</Table.HeaderCell>
+                  <Table.HeaderCell>Aprovado</Table.HeaderCell>
+                  <Table.HeaderCell>Arquivado</Table.HeaderCell>
                 </Table.Row>
               </Table.Header>
-
               <Table.Body>
-                {_.map(paginatedItems, ({projeto, assunto, vereador, data_aprovacao, data_arquivamento}) => (
-                  <Table.Row key={projeto}>
-                    <Table.Cell textAlign='center'>{projeto}</Table.Cell>
-                    <Table.Cell>{assunto}</Table.Cell>
-                    <Table.Cell>{vereador.nome} ({vereador.apelino})</Table.Cell>
-                    <Table.Cell textAlign='center'>{statusProjeto(data_aprovacao)}</Table.Cell>
-                    <Table.Cell textAlign='center'>{statusProjeto(data_arquivamento)}</Table.Cell>
+                {projetos.map(item => (
+                  <Table.Row key={item.id}>
+                    <Table.Cell>{item.projeto}</Table.Cell>
+                    <Table.Cell textAlign='left'>{item.assunto}</Table.Cell>
+                    <Table.Cell>{this.getVereador(item)}</Table.Cell>
+                    <Table.Cell>{this.statusProjeto(item.data_aprovacao)}</Table.Cell>
+                    <Table.Cell>{this.statusProjeto(item.data_arquivamento)}</Table.Cell>
                   </Table.Row>
                 ))}
               </Table.Body>
+              <Table.Footer fullWidth>
+                <Table.Row textAlign='center' verticalAlign='middle'>
+                  <Table.HeaderCell colSpan='5'>
+                    <MyPaginacao items={items} onClick={this.carregarDados}></MyPaginacao>
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Footer>
             </Table>
-            <div className="centered">
-              <Pagination 
-                defaultActivePage={1}
-                totalPages={this.state.paginas} 
-                onPageChange={this.handlePageClick}
-                siblingRange={1}
-                boundaryRange={0}
-                ellipsisItem={null}
-              />
-            </div>
           </div>
         );
       }
